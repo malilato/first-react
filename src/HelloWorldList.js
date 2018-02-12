748db05bbb3aa2fa9b6c3ab106d33bdca5fce356
malilato <malilato@arcada.fi>
2018-02-12 21:09:14 +0200

    added more data

--- src/HelloWorldList.js
+++ src/HelloWorldList.js
@@ -6,7 +6,10 @@ import AddGreeter from './AddGreeter';
 class HelloWorldList extends Component {
   constructor(props) {
   super(props);
-  this.state = { greetings: [{id: 13,name: "Jimbo", email: "jim@cs.es", phonenumber: "123123"}, {id: 22, name: "Sally", email: "sally@cd.ed", phonenumber: "123123"}] };
+  this.state = { greetings: [{id: 13,name: "Jimbo", email: "jim@cs.es", phonenumber: "123123"}, {id: 22, name: "Sally", email: "sally@cd.ed", phonenumber: "123123"}
+, {id: 3, name: "Maria Anders", email: "maria@anders.ed", phonenumber: "050123123"}, {id: 5, name: "Francisco Chang", email: "chang@changder.fi", phonenumber: "04012331"},
+{id: 34, name: "Helen Bennett", email: "helen@bennet.fi", phonenumber: "044070070"}, {id: 41, name: "Giovanni Rovelli", email: "rovelli@gio.fi", phonenumber: "050735152"},
+{id: 81, name: "Yoshi Tannamuri", email: "yosha@yoshi.fi", phonenumber: "040123213"}, {id: 89, name: "Bill Tanner", email: "bill@tanner.fi", phonenumber: "050555666"}] };
   this.addGreeting = this.addGreeting.bind(this);
   this.removeGreeting = this.removeGreeting.bind(this);
   }
